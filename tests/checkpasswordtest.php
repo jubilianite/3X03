a9712352bd9dc0c1b2abfa5c5d51f05c519bd3f0
jubilianite <jubilianharvest@gmail.com>
2022-11-12 02:48:43 +0800

    Update checkpasswordtest.php

--- tests/checkpasswordtest.php
+++ tests/checkpasswordtest.php
@@ -25,6 +25,6 @@ class MyScriptTest extends PHPUnit_Framework_TestCase {
     public function testMyFunction() {
         //include_once 'checkpassword.php';
         $result = checkpassword("password");
-        $this->assertEquals("Bad Password", $result);
+        $this->assertTrue($result == "Bad Password");
     }
 }
