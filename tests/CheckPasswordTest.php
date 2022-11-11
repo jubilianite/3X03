2a3d78f0fd599d7f15731cdc18c344a62431b635
jubilianite <jubilianharvest@gmail.com>
2022-11-12 03:16:01 +0800

    Update CheckPasswordTest.php

--- tests/CheckPasswordTest.php
+++ tests/CheckPasswordTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 class CheckPasswordTest extends TestCase {
     public function testCheckPassword() {
 		$path = dirname( dirname(__FILE__) );
-        include_once $path . 'CheckPassword.php';
+        include_once $path . '/CheckPassword.php';
         $result = CheckPassword("password");
         $this->assertEquals('Bad Password', $result);
     }
