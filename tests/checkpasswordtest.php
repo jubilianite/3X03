167a8d25064c3f7604522726bd1523a0f5754e4e
jubilianite <jubilianharvest@gmail.com>
2022-11-12 02:35:22 +0800

    gg

---
+++ tests/checkpasswordtest.php
@@ -0,0 +1,31 @@
+<?php
+use PHPUnit\Framework\TestCase;
+//require 'checkpassword.php';
+
+//class GumballMachineTest extends TestCase {
+//
+//	public $gumballMachineInstance;
+//	
+//	public function setup():void {
+//		
+//		$this->gumballMachineInstance = new GumballMachine();
+//	}
+//	
+//	public function testIfWheelWorks() {
+//		
+//		$this->gumballMachineInstance->setGumballs(100);
+//		
+//		$this->gumballMachineInstance->turnWheel();
+//		
+//		$this->assertEquals(99, $this->gumballMachineInstance->getGumballs());
+//	}
+//}
+
+class MyScriptTest extends PHPUnit_Framework_TestCase {
+    public function testMyFunction() {
+        include_once '../checkpassword.php';
+        $result = checkpassword("password");
+
+        $this->assertEquals("Bad Password", $result);
+    }
+}
