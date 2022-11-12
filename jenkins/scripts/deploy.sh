3f534a1e09f4bcae8da5c6853c07324fbb610f69
jubilianite <jubilianharvest@gmail.com>
2022-11-13 02:22:20 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:8000 --ip 172.18.0.4--name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
+docker run -d -p 80:8000 --ip 172.18.0.4 --name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
 docker exec -it --user root my-apache-php-app apt-get update
 docker exec -it --user root my-apache-php-app apt-get upgrade
 docker exec -it --user root my-apache-php-app apt install net-utils
