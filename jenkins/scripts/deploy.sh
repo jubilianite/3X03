59bd8a72ef454d639dbede9e5785977338a3e229
jubilianite <jubilianharvest@gmail.com>
2022-11-13 22:14:27 +0800

    xx

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,11 +4,11 @@ set -x
 
 #docker run -d -p 80:8000 --name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
 sudo su root
-cd /var/jenkins_home/3X03 && git pull
-chmod +x jenkins/scripts/deploy.sh
-chmod +x jenkins/scripts/kill.sh
-cp /var/jenkins_home/3X03 /var/www/html
-service apache2 start
+cd /var/jenkins_home/3X03 && sudo git pull
+sudo chmod +x jenkins/scripts/deploy.sh
+sudo chmod +x jenkins/scripts/kill.sh
+sudo cp /var/jenkins_home/3X03 /var/www/html
+sudo service apache2 start
 
 sleep 1
 
@@ -16,4 +16,3 @@ set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -3,5 +3,5 @@
 set -x
 #docker kill my-apache-php-app
 #docker rm my-apache-php-app
-service apache2 stop
-rm /var/www/html/*
+sudo service apache2 stop
+sudo rm /var/www/html/*
