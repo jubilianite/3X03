507dc46b2d849920a3cfdde885973a11f5f1aae2
jubilianite <jubilianharvest@gmail.com>
2022-11-13 21:51:04 +0800

    new

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:8000 --name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
-docker exec -i --user root my-apache-php-app apt-get update
-docker exec -i --user root my-apache-php-app apt-get upgrade
-docker exec -i --user root my-apache-php-app apt install net-utils
-docker exec -i --user root my-apache-php-app apt install net-tools
-docker exec -i --user root my-apache-php-app apt install iputils-ping
+
+#docker run -d -p 80:8000 --name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
+sudo su root
+cd /var/jenkins_home/3X03 && git pull
+chmod +x jenkins/scripts/deploy.sh
+chmod +x jenkins/scripts/kill.sh
+cp /var/jenkins_home/3X03 /var/www/html
+service apache2 start
+
 sleep 1
+
 set +x
 
 echo 'Now...'
--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker kill my-apache-php-app
-docker rm my-apache-php-app
+#docker kill my-apache-php-app
+#docker rm my-apache-php-app
+service apache2 stop
+rm /var/www/html/*
